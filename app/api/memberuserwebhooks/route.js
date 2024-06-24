7be8180582ee333b56079d4ea1c71501c8bfa728
Kostiantyn1303 <kosty13humera@gmail.com>
2024-06-24 18:31:25 +0200

    secret

--- app/api/memberuserwebhooks/route.js
+++ app/api/memberuserwebhooks/route.js
@@ -20,9 +20,7 @@ export async function POST(req) {
     },
   });
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    throw new Error("Please add WEBHOOK_SECRET to .env or .env.local");
   }
 
   // Get the headers
