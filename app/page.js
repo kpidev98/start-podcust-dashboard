bfbd647ae3eb726ff1ca5b0de150ba778237ee4d
Kostiantyn1303 <kosty13humera@gmail.com>
2024-07-26 17:13:52 +0200

    indexing

--- app/page.js
+++ app/page.js
@@ -2,6 +2,10 @@ import DashboardComponent from "@/components/DashboardPageCom";
 export const metadata = {
   title: "Dashboard",
   description: "Create your projects with Start Podcust",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 export default function Home() {
   return (
